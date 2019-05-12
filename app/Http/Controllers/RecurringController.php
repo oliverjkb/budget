70459ee22fafe0baaf1bf3303fce5fba6d0df361
Oliver Jakobi <41308341+oliverjkb@users.noreply.github.com>
2019-05-12 21:00:27 +0200

    Issue 16 recurring transactions (#1)
    * Replace date validation of recurring transactions to make transactions possible at every day of month

--- app/Http/Controllers/RecurringController.php
+++ app/Http/Controllers/RecurringController.php
@@ -32,7 +32,9 @@ class RecurringController extends Controller {
 
     public function store(Request $request) {
         $request->validate([
-            'day' => 'required|integer|between:1,28',
+            'day' => array('required',
+                           'regex:/\b(0?[1-9]|[12][0-9]|3[01])\b/',
+		       	          ),
             'end' => 'nullable|date|date_format:Y-m-d',
             'tag' => 'nullable|exists:tags,id', // TODO CHECK IF TAG BELONGS TO USER
             'description' => 'required|max:255',
@@ -45,7 +47,7 @@ class RecurringController extends Controller {
 
         $recurring->space_id = session('space')->id;
         $recurring->type = 'monthly';
-        $recurring->day = $request->input('day');
+        $recurring->day = ltrim($request->input('day'), '0');
         $recurring->starts_on = date('Y-m-d');
         $recurring->ends_on = $request->input('end');
         $recurring->tag_id = $request->input('tag');
