75354407ec138ffc65c65c911efb94a735284014
Oliver Jakobi <oliver@jakobi.wtf>
2019-05-30 17:56:39 +0200

    added some german translations

---
+++ resources/lang/de/fields.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'name' => 'Name',
+
+    'avatar' => 'Avatar',
+    'email' => 'E-Mail',
+    'password' => 'Passwort',
+    'language' => 'Sprache',
+    'theme' => 'Theme',
+    'currency' => 'Währung',
+    'weekly_report' => 'Wöchentlicher Report',
+
+    'date' => 'Datum',
+    'description' => 'Bechreibung',
+    'amount' => 'Betrag',
+
+    'file' => 'Datei'
+];
---
+++ resources/lang/de/models.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'tags' => 'Label',
+    'tag' => 'Label',
+
+    'recurrings' => 'Wiederkehrende',
+    'recurring' => 'Wiederkehrende',
+
+    'earnings' => 'Einnahmen',
+    'earning' => 'Einnahme',
+
+    'spendings' => 'Ausgaben',
+    'spending' => 'Ausgabe',
+
+    'imports' => 'Imports',
+    'import' => 'Import'
+];
